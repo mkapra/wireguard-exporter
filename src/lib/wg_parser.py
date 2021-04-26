0283bc5fee389b7dadb2ae49cc1782add04b8539
Florian Fink <florianfink85@gmail.com>
2021-04-26 21:18:05 +0200

    add pycache to gitignore, working wg show all dump output parser, refactored code with pylint

---
+++ src/lib/wg_parser.py
@@ -0,0 +1,52 @@
+"""
+Imports
+
+:subprocess: run wg show all dump command on shell
+"""
+import subprocess
+
+
+# pylint: disable=too-few-public-methods
+class WGParser:
+    """
+    Parses output of wg show all dump
+    """
+
+    @staticmethod
+    def parse_output(test=False):
+        """
+        parses output of wg show all dump
+
+        :param: test: get actual data or get 'fake' data from the file test/out.txt
+        :return: list of dictionaries
+        """
+
+        command = ['wg', 'show', 'all', 'dump']
+        if test:
+            command = ['cat', 'test/wg_dump_test_out.txt']
+
+        with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as child:
+
+            lines = child.stdout.readlines()
+            # we don't need the first line since it is related to the peer
+            # on which the command was run.
+            lines.pop(0)
+
+            keys = ['interface', 'public-key', 'preshared-key', 'endpoint', 'allowed-ips',
+                    'latest-handshake', 'transfer-rx', 'transfer-tx', 'persistent-keepalive']
+            ignore_keys = ['persistent-keepalive', 'preshared-key']
+            result = []
+
+            for line in lines:
+                fields = line.strip().split(b'\t')
+                res = dict()
+                for i, key in enumerate(keys):
+                    if key in ignore_keys:
+                        continue
+                    if fields[i].decode() == "(none)":
+                        res[key] = ""
+                    else:
+                        res[key] = fields[i].decode()
+                result.append(res)
+
+            return result
