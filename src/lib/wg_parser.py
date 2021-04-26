ece0699ae263faefd8dde2be03ca64932e90a3dc
Florian Fink <florianfink85@gmail.com>
2021-04-26 23:26:32 +0200

    return actual data from collectors

--- src/lib/wg_parser.py
+++ src/lib/wg_parser.py
@@ -12,15 +12,16 @@ class WGParser:
     Parses output of wg show all dump
     """
 
-    @staticmethod
-    def parse_output(test=False):
+    def __init__(self):
+        self.parse_result = []
+
+    def parse_output(self, test=False):
         """
         parses output of wg show all dump
 
         :param: test: get actual data or get 'fake' data from the file test/out.txt
         :return: list of dictionaries
         """
-
         command = ['wg', 'show', 'all', 'dump']
         if test:
             command = ['cat', 'test/wg_dump_test_out.txt']
@@ -28,18 +29,24 @@ class WGParser:
         with subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT) as child:
 
             lines = child.stdout.readlines()
-            # we don't need the first line since it is related to the peer
-            # on which the command was run.
-            lines.pop(0)
 
             keys = ['interface', 'public-key', 'preshared-key', 'endpoint', 'allowed-ips',
                     'latest-handshake', 'transfer-rx', 'transfer-tx', 'persistent-keepalive']
+            device_keys = ['interface', 'private-key', 'public-key', 'listen-port', 'fwmark']
             ignore_keys = ['persistent-keepalive', 'preshared-key']
             result = []
+            device_result = []
 
             for line in lines:
                 fields = line.strip().split(b'\t')
                 res = dict()
+
+                if len(fields) == 5:
+                    for i, key in enumerate(device_keys):
+                        res[key] = fields[i].decode()
+                    device_result.append(res)
+                    continue
+
                 for i, key in enumerate(keys):
                     if key in ignore_keys:
                         continue
@@ -49,4 +56,4 @@ class WGParser:
                         res[key] = fields[i].decode()
                 result.append(res)
 
-            return result
+            self.parse_result = result, device_result
--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 """
 Imports
 
-:random: For sample data output
 :time: control sleep time in main function
 """
-import random
 import time
+from datetime import datetime, timedelta
 
 from prometheus_client import start_http_server, PROCESS_COLLECTOR, PLATFORM_COLLECTOR
-from prometheus_client.core import REGISTRY, CounterMetricFamily
+from prometheus_client.core import REGISTRY, GaugeMetricFamily
 
 import lib.wg_parser as wg_parser
 
@@ -24,18 +23,23 @@ class CollectSendBytesTotal:
     """
     Custom collector class for bytes sent to wireguard peer
     """
-    @staticmethod
-    def collect():
+
+    def __init__(self, wgparser: wg_parser.WGParser):
+        self.parser = wgparser
+
+    def collect(self):
         """
         collects metrics
 
         :return: one metric collection per public_key
         """
-        metric_collection = CounterMetricFamily('wireguard_sent_bytes_total',
-                                                'Bytes sent to the peer',
-                                                labels=['interface', 'public_key', 'allowed_ips'])
-        for i in range(5):
-            metric_collection.add_metric(['wg0', str(i), '192.168.0.100/32'], random.random())
+        result, device_result = self.parser.parse_result
+        metric_collection = GaugeMetricFamily('wireguard_sent_bytes_total',
+                                              'Bytes sent to the peer',
+                                              labels=['interface', 'public_key', 'allowed_ips'])
+        for peer_dict in result:
+            metric_collection.add_metric([peer_dict['interface'], peer_dict['public-key'],
+                                          peer_dict['allowed-ips']], peer_dict['transfer-tx'])
         yield metric_collection
 
 
@@ -44,18 +48,23 @@ class CollectRecvBytesTotal:
     """
     Custom collector class for bytes received from wireguard peer
     """
-    @staticmethod
-    def collect():
+
+    def __init__(self, wgparser: wg_parser.WGParser):
+        self.parser = wgparser
+
+    def collect(self):
         """
         collects metrics
 
         :return: one metric collection per public_key
         """
-        metric_collection = CounterMetricFamily('wireguard_received_bytes_total',
-                                                'Bytes received from the peer',
-                                                labels=['interface', 'public_key', 'allowed_ips'])
-        for i in range(5):
-            metric_collection.add_metric(['wg0', str(i), '192.168.0.100/32'], random.random())
+        result, device_result = self.parser.parse_result
+        metric_collection = GaugeMetricFamily('wireguard_received_bytes_total',
+                                              'Bytes received from the peer',
+                                              labels=['interface', 'public_key', 'allowed_ips'])
+        for peer_dict in result:
+            metric_collection.add_metric([peer_dict['interface'], peer_dict['public-key'],
+                                          peer_dict['allowed-ips']], peer_dict['transfer-rx'])
         yield metric_collection
 
 
@@ -64,29 +73,98 @@ class CollectLatestHandshakeSeconds:
     """
     Custom collector class for how many seconds ago the last wireguard handshake occured
     """
-    @staticmethod
-    def collect():
+
+    def __init__(self, wgparser: wg_parser.WGParser):
+        self.parser = wgparser
+
+    def collect(self):
         """
         collects metrics
 
         :return: one metric collection per public_key
         """
-        metric_collection = CounterMetricFamily('wireguard_latest_handshake_seconds',
-                                'Seconds from the last handshake',
-                                labels=['interface', 'public_key', 'allowed_ips'])
-        for i in range(5):
-            metric_collection.add_metric(['wg0', str(i), '192.168.0.100/32'], random.random())
+        result, device_result = self.parser.parse_result
+        metric_collection = GaugeMetricFamily('wireguard_latest_handshake_seconds',
+                                              'Seconds from the last handshake',
+                                              labels=['interface', 'public_key', 'allowed_ips'])
+        for peer_dict in result:
+            metric_collection.add_metric([peer_dict['interface'], peer_dict['public-key'],
+                                          peer_dict['allowed-ips']], peer_dict['latest-handshake'])
+        yield metric_collection
+
+
+# pylint: disable=too-few-public-methods
+class CollectPeerInfo:
+    """
+    Custom collector class for how many seconds ago the last wireguard handshake occured
+    """
+
+    def __init__(self, wgparser: wg_parser.WGParser):
+        self.parser = wgparser
+
+    def collect(self):
+        """
+        collects metrics
+
+        :return: one metric collection per public_key
+        """
+        result, device_result = self.parser.parse_result
+        metric_collection = GaugeMetricFamily('wireguard_peer_info',
+                                              'WireGuard Peer Info',
+                                              labels=['interface', 'public_key', 'allowed_ips'])
+        for peer_dict in result:
+            local_time = datetime.fromtimestamp(int(peer_dict['latest-handshake']))
+            if datetime.now() - local_time < timedelta(minutes=10):
+                online_state = 1
+            else:
+                online_state = 0
+            metric_collection.add_metric([peer_dict['interface'], peer_dict['public-key'],
+                                          peer_dict['allowed-ips']], int(online_state))
+        yield metric_collection
+
+
+# pylint: disable=too-few-public-methods
+class CollectDeviceInfo:
+    """
+    Custom collector class for how many seconds ago the last wireguard handshake occured
+    """
+
+    def __init__(self, wgparser: wg_parser.WGParser):
+        self.parser = wgparser
+
+    def collect(self):
+        """
+        collects metrics
+
+        :return: one metric collection per public_key
+        """
+        result, device_result = self.parser.parse_result
+        metric_collection = GaugeMetricFamily('wireguard_device_info',
+                                              'WireGuard device Info',
+                                              labels=['interface', 'public_key'])
+        for device_dict in device_result:
+            print(device_dict)
+            metric_collection.add_metric([device_dict['interface'],
+                                          device_dict['public-key']], int(123))
+
         yield metric_collection
 
 
 if __name__ == '__main__':
-    start_http_server(8000)
-    REGISTRY.register(CollectSendBytesTotal())
-    REGISTRY.register(CollectRecvBytesTotal())
-    REGISTRY.register(CollectLatestHandshakeSeconds())
 
     parser = wg_parser.WGParser()
-    parser.parse_output(test=True)
+    parser.parse_output()
+    print("Started parser")
+
+    start_http_server(8000)
+    REGISTRY.register(CollectSendBytesTotal(parser))
+    REGISTRY.register(CollectRecvBytesTotal(parser))
+    REGISTRY.register(CollectLatestHandshakeSeconds(parser))
+    REGISTRY.register(CollectPeerInfo(parser))
+    print("Registering CollectDeviceInfo collector")
+    REGISTRY.register(CollectDeviceInfo(parser))
+    print("Registered Collectors")
 
     while True:
         time.sleep(1)
+        parser.parse_output(test=True)
